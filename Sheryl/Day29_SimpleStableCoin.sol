8697fbbdb799da1144705beba07ceb7b8b2e9d9c
Sheryl0907 <sherylyao0907@gmail.com>
2026-03-28 17:38:36 +0800

    Update Day29_SimpleStableCoin.sol

--- Sheryl/Day29_SimpleStableCoin.sol
+++ Sheryl/Day29_SimpleStableCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.so";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
